616db2e3b345ef13d18b96961000111704a0dde4
Rasit Evduzen <r.evduzen@hotmail.com>
2024-08-02 23:25:40 +0300

    Update

--- CentralLimitTheorem/Proof2/main.m
+++ CentralLimitTheorem/Proof2/main.m
@@ -16,18 +16,20 @@ for n = 1:num_iterations
     fixed_signal = conv_result;
     conv_result = zeros(1,num_samples);
     for k = 1:length(t)
-        shifted_signal2 = circshift(fliplr(signal2), [0, k-num_samples/2]);
+        shifted_signal2 = circshift(fliplr(signal2), [0, k-num_samples/2]); % Circular Convolution
         conv_result(k) = sum(fixed_signal.*shifted_signal2)*(t(2)-t(1));
-        clf
-        plot(t, fixed_signal, 'b', 'LineWidth', 2),,hold on
-        plot(t, shifted_signal2, 'r', 'LineWidth', 2);
-        plot(t, conv_result, 'y', 'LineWidth', 2);
-        x_patch = [t, fliplr(t)];
-        y_patch = [zeros(1, num_samples), fliplr(conv_result)];
-        patch(x_patch, y_patch, [.1 .1 .1], 'FaceAlpha', 0.4, 'EdgeColor', 'none');
-        axis([-5 5 -0.1 1.1]);yline(0, 'LineWidth', 2);
-        title("Central Limit Theorem")
-        legend('Previous Convolution Result', 'Step Function', 'New Convolution Result', 'Function Area');
-        drawnow;
+        if mod(k,10) == 0
+            clf
+            plot(t, fixed_signal, 'b', 'LineWidth', 2),hold on
+            plot(t, shifted_signal2, 'r', 'LineWidth', 2);
+            plot(t, conv_result, 'y', 'LineWidth', 2);
+            x_patch = [t, fliplr(t)];
+            y_patch = [zeros(1, num_samples), fliplr(conv_result)];
+            patch(x_patch, y_patch, [.1 .1 .1], 'FaceAlpha', 0.4, 'EdgeColor', 'none');
+            axis([-5 5 -0.1 1.1]);yline(0, 'LineWidth', 2);
+            title("Central Limit Theorem")
+            legend('Previous Convolution Result', 'Step Function', 'New Convolution Result', 'Function Area');
+            drawnow;
+        end
     end
 end
